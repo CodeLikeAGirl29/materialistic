ee1a0be8e6e2bad0ab7322c0bff7fbaf557bca45
codelikeagirl29 <miss_leading009@yahoo.com>
2023-02-22 19:33:51 -0500

    some data changes

---
+++ assets/mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from your site 'vCard & Resume'"; //letter subject
+	$to = 'admin@lindseyk.dev'; // Enter Your E-mail
+	$from = 'admin@lindseyk.dev'; // Admin e-mail
+	$fromName = 'Lindseyk'; // Your company name
+	$charset = 'UTF-8';
+?>
